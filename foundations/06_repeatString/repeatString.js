605773eb2d448377abdd7805b8fc43ec1af0e884
David Omar <davidomartech@tutamail.com>
2026-01-18 12:19:40 -0500

    Complete repeatString and reverseString exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, repeatBy) {
+  if (repeatBy === 1) return str;
+  if (repeatBy === 0 || str === "") return "";
+  if (repeatBy < 0) return "ERROR";
 
+  let output = "";
+  for (let i = 0; i < repeatBy; ++i) {
+    output += str;
+  }
+  return output;
 };
 
 // Do not edit below this line
