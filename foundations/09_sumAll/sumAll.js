2f7930deaff295931a65b5fb5f9dfcdc1aa7242c
David Omar <davidomartech@tutamail.com>
2026-01-18 12:52:03 -0500

    Complete removeFromArray and sumAll exercises

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (startRange, endRange) {
+  if (!Number.isInteger(startRange) || !Number.isInteger(endRange))
+    return "ERROR";
+  if (startRange < 0 || endRange < 0) return "ERROR";
 
+  if (endRange < startRange) [startRange, endRange] = [endRange, startRange];
+  let loopLength = endRange - startRange;
+  let sum = startRange;
+  for (let i = startRange + 1; i <= endRange; ++i) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
