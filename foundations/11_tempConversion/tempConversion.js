443f872bc83f8b89f833377fe782e349dc649880
David Omar <davidomartech@tutamail.com>
2026-01-19 09:43:14 -0500

    Complete leapYears and tempConversion exercises

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let conversionC = (temp - 32) * (5 / 9);
+  return conversionC.toFixed(1) * 1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let conversionF = temp * (9 / 5) + 32;
+  return conversionF.toFixed(1) * 1;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
